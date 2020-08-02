b89ee8fd4b36d37f8ae07d283a7904270fda0477
Davis Hoang <davishoang96@gmail.com>
2020-08-02 23:36:37 +0800

    Clean up

---
+++ src/Components/Todos.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+function Todos() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>This is Todos App</h2>
+      </header>
+    </div>
+  );
+}
+
+export default Todos;

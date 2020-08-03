f4f63d4d3e32467f3f761f03c25231206892eec5
Davis Hoang <davishoang96@gmail.com>
2020-08-03 14:42:24 +0800

    Router

--- src/App.js
+++ src/App.js
@@ -1,54 +1,56 @@
-import React, { Component } from 'react';
-import Todos from './Components/Todos'
-import { render } from '@testing-library/react';
-import Header from './Components/layouts/Header'
-import AddTodo from './Components/AddTodo';
-import {v4 as uuid} from 'uuid';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Todos from "./Components/Todos";
+import { render } from "@testing-library/react";
+import Header from "./Components/layouts/Header";
+import AddTodo from "./Components/AddTodo";
+import { v4 as uuid } from "uuid";
+import About from "./Components/pages/About";
 
 export class App extends Component {
-
   state = {
     todos: [
       {
         id: uuid(),
         title: "Clean the floor",
-        completed: false
+        completed: false,
       },
       {
         id: uuid(),
         title: "Ask for Phd Schoolarship",
-        completed: false
+        completed: false,
       },
       {
         id: uuid(),
         title: "Send email to Brianna",
-        completed: false
+        completed: false,
       },
       {
         id: uuid(),
         title: "Complete the paragraph",
-        completed: false
-      }
-    ]
-  }
+        completed: false,
+      },
+    ],
+  };
 
   // toggle completion for todo list item
   markCompleted = (id) => {
-    this.setState({todos: this.state.todos.map(
-      todo => {
-        if (todo.id === id){
-          todo.completed = !todo.completed
+    this.setState({
+      todos: this.state.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
         }
         return todo;
-      }
-    )})
-  }
-
+      }),
+    });
+  };
 
   // Delete todo item
   delTodo = (id) => {
-    this.setState({todos: [...this.state.todos.filter(todo => todo.id !== id )]})
-  }
+    this.setState({
+      todos: [...this.state.todos.filter((todo) => todo.id !== id)],
+    });
+  };
 
   // Add todo item
   AddTodo = (title) => {
@@ -56,26 +58,38 @@ export class App extends Component {
       id: uuid(),
       title: title,
       completed: false,
-    }
-    this.setState({todos: [...this.state.todos, newTodo]})
-  }
-
+    };
+    this.setState({ todos: [...this.state.todos, newTodo] });
+  };
 
   render() {
-    console.log(this.state.todos)
+    console.log(this.state.todos);
     return (
-      
-      <div className="App">
-        <header className="App-header">
-          <Header />
-          <AddTodo AddTodo={this.AddTodo} />
-          <Todos todos={this.state.todos} markCompleted={this.markCompleted} delTodo={this.delTodo} />
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <Header />
+            <Route
+              exact
+              path="/"
+              render={(props) => (
+                <React.Fragment>
+                  <AddTodo AddTodo={this.AddTodo} />
+                  <Todos
+                    todos={this.state.todos}
+                    markCompleted={this.markCompleted}
+                    delTodo={this.delTodo}
+                  />
+                </React.Fragment>
+              )}
+            />
 
-        </header>
-      </div>
-    )
+            <Route path="/about" component={About}></Route>
+          </header>
+        </div>
+      </Router>
+    );
   }
 }
 
-export default App
-
+export default App;
--- src/Components/layouts/Header.js
+++ src/Components/layouts/Header.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 export default class Header extends Component {
   render() {
     return (
       <div style={headerStyle}>
         <h1> Todo list </h1>
+        <Link style={{color: 'white'}} to="/">Home</Link> | <Link style={{color: 'white'}} to="/about">About</Link>
       </div>
     )
   }
